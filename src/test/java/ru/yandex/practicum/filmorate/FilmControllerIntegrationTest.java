7875281e3a5f421929f74e23303dc8aa1de55984
LacusFaustus <122123740+LacusFaustus@users.noreply.github.com>
2025-09-13 20:20:06 +0300

    fix: correct package structure and imports

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerIntegrationTest.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.controller;
+package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.controller;
+package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
