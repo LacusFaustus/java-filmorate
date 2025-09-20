d59516ee9270d70417368717624b038ea2875557
LacusFaustus <122123740+LacusFaustus@users.noreply.github.com>
2025-09-20 09:41:40 +0300

    fix: исправлены checkstyle ошибки - добавлены пустые строки между методами в интерфейсах

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmStorage {
+    List<Film> getAllFilms();
+
+    Film createFilm(Film film);
+
+    Film updateFilm(Film film);
+
+    Optional<Film> getFilmById(int id);
+
+    void deleteFilm(int id);
+
+    boolean filmExists(int id);
+}
